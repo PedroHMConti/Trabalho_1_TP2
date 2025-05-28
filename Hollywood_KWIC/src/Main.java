64c285ea108824fe8e1249f6a2b7e5cde3c7c19c
Pedro Conti <pedrohmconti@gmail.com>
2025-05-27 21:58:30 -0300

    Update Main.java

--- Hollywood_KWIC/src/Main.java
+++ Hollywood_KWIC/src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
+        //input do programa
+        Scanner sc = new Scanner(System.in);
+        EscreveInputFile escreveInputFile = new EscreveInputFile();
+        String linha;
+        while(!(linha = sc.nextLine()).equalsIgnoreCase("fim")){
+            escreveInputFile.escreveNoArquivoInput(linha);
+        }
+
         WordFrequencyFramework wfapp = new WordFrequencyFramework();
         StopWordFilter stopWordFilter = new StopWordFilter(wfapp);
         DataStorage dataStorage = new DataStorage(wfapp,stopWordFilter);
         KeyWordContextGenerator keyWordContextGenerator  = new KeyWordContextGenerator(wfapp,dataStorage);
-        wfapp.run("C:\\Users\\pedro\\OneDrive\\Área de Trabalho\\projetos_java\\Hollywood_wordCount\\src\\Files\\inputFile.txt");
+        wfapp.run("C:\\Users\\pedro\\OneDrive\\Área de Trabalho\\projetos_java\\Hollywood_KWIC\\src\\Files\\inputFile.txt");
     }
 }
