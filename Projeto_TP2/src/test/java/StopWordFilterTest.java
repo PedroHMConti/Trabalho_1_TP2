ceb73d3dd7f35038c9f1e1feb39bd086aa9703c6
Vítor Matos <vitormatosg@gmail.com>
2025-05-30 00:21:18 -0300

    Adiciona novos testes

--- Projeto_TP2/src/test/java/StopWordFilterTest.java
+++ Projeto_TP2/src/test/java/StopWordFilterTest.java
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
-
 class StopWordFilterTest {
     @Test
     void testIsStopWordInstrucao() {
