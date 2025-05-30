d638574914e645cf27ab7321a65364e9745335dd
felipe-costa-ft <felipecosta@ieee.org>
2025-05-30 11:03:04 -0300

    Ajustes UI

--- Projeto_TP2/src/main/java/Main.java
+++ Projeto_TP2/src/main/java/Main.java
@@ -1,8 +1,6 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import java.nio.file.Paths;
-
 public class Main{
     public static void main(String[] args) {
         //input do programa
